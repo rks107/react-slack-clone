9ce8b958337f95fe05ef6ab528fa31505531d8aa
ROHIT KUMAR <sumitkumar@rohit-2.local>
2020-09-14 21:33:07 +0530

    Add Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { SignIn } from './';
+import {Switch, Route} from 'react-router-dom';
+
+function Home() {
+  return <div> Home </div>
+}
 
 function App() {
   return (
     <div className="App">
       Slack
-      <SignIn />
+      <Switch>
+        <Route exact path="/" component={SignIn} />
+        <Route exact path="/home" component={Home} />
+      </Switch>
     </div>
   );
 }
